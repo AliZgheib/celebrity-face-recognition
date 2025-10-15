cea7152408bbba34e4c3baf555dd201bc48a730a
AliZgheib <zgheibali@gmail.com>
2025-10-15 17:32:21 +0300

    Improve code structure in the AWS CDK

--- back-end/lib/back-end-stack.ts
+++ back-end/lib/back-end-stack.ts
@@ -1,63 +1,40 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-
-import * as path from "path";
-
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as iam from "aws-cdk-lib/aws-iam";
-import { Duration } from "aws-cdk-lib";
 
 export class BackEndStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
+    // API Gateway
     const api = new apigateway.RestApi(this, "rekognition-api", {
-      description: "api gateway",
-      deployOptions: {
-        stageName: "dev",
-      },
-      // 👇 enable CORS
+      description: "Celebrity Recognition API",
+      deployOptions: { stageName: "dev" },
       defaultCorsPreflightOptions: {
-        allowHeaders: ["Content-Type"],
-        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
-        allowOrigins: ["*"],
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
       },
     });
 
-    // 👇 create an Output for the API URL
-    new cdk.CfnOutput(this, "apiUrl", { value: api.url });
-
-    // 👇 define the lambda function
+    // Lambda Function
     const rekognitionLambda = new lambda.Function(this, "rekognition-lambda", {
       runtime: lambda.Runtime.NODEJS_22_X,
       handler: "index.main",
-      code: lambda.Code.fromAsset(path.join(__dirname, "/../src/rekognition")),
-      timeout: Duration.seconds(29),
+      code: lambda.Code.fromAsset("src/rekognition"),
+      timeout: cdk.Duration.seconds(29),
+      initialPolicy: [
+        new iam.PolicyStatement({
+          actions: ["rekognition:RecognizeCelebrities"],
+          resources: ["*"],
+        }),
+      ],
     });
 
-    // 👇 create a policy statement
-    const rekognitionLambdaPolicy = new iam.PolicyStatement({
-      actions: ["rekognition:RecognizeCelebrities"],
-      resources: ["*"],
-    });
-
-    // 👇 add the policy to the Function's role
-    rekognitionLambda.role?.attachInlinePolicy(
-      new iam.Policy(this, "rekognition-lambda-policy", {
-        statements: [rekognitionLambdaPolicy],
-      })
-    );
-
-    // 👇 add a /rekognition resource
-    const rekognitionResource = api.root.addResource("rekognition");
-
-    // 👇 integrate POST /rekognition with rekognitionLambda
-    rekognitionResource.addMethod(
-      "POST",
-      new apigateway.LambdaIntegration(rekognitionLambda, { proxy: true })
-    );
+    // API Route
+    api.root
+      .addResource("rekognition")
+      .addMethod("POST", new apigateway.LambdaIntegration(rekognitionLambda));
   }
 }
